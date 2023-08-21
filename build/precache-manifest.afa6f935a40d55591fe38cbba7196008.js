04604773cb22bbf02b73b9c02905f09a619fa49d
Arya Shapouri <arya@shapouri.com>
2023-08-21 18:43:07 -0400

    Backend + Frontend

---
+++ build/precache-manifest.afa6f935a40d55591fe38cbba7196008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b13ca07b99f507c16c9",
+    "url": "/static/css/main.7b702faa.chunk.css"
+  },
+  {
+    "revision": "6b13ca07b99f507c16c9",
+    "url": "/static/js/main.71f3f2d3.chunk.js"
+  },
+  {
+    "revision": "c2a4eb1306cf1afe55d9",
+    "url": "/static/js/runtime~main.f7f764ff.js"
+  },
+  {
+    "revision": "cade6947d9e273a117cf",
+    "url": "/static/css/2.fbe69517.chunk.css"
+  },
+  {
+    "revision": "cade6947d9e273a117cf",
+    "url": "/static/js/2.6645bddf.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "d95470c198d1a26301b7d5fbc2aff1fe",
+    "url": "/index.html"
+  }
+];
