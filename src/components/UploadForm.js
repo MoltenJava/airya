be193fd74cef3e095e643f4e0c09d2739c548cfe
MoltenJava <arya@shapouri.com>
2023-08-22 11:47:16 -0400

    Fixed conflicts from Heroku deployment

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import axios from 'axios';
-=======
-import { Button, Form } from 'react-bootstrap';
-import './UploadForm.css';
->>>>>>> tmp
 
 const UploadForm = () => {
     const [songTitle, setSongTitle] = useState('');
@@ -15,13 +10,8 @@ const UploadForm = () => {
     const [labelCopyPDF, setLabelCopyPDF] = useState(null);
     const [artwork, setArtwork] = useState(null);
 
-<<<<<<< HEAD
     const handleFormSubmit = async (event) => {
         event.preventDefault();
-=======
-    const handleSubmit = async (e) => {
-        e.preventDefault();
->>>>>>> tmp
 
         const formData = new FormData();
         formData.append('songTitle', songTitle);
@@ -46,7 +36,6 @@ const UploadForm = () => {
     };
 
     return (
-<<<<<<< HEAD
         <form onSubmit={handleFormSubmit}>
             <input type="text" placeholder="Song Title" value={songTitle} onChange={e => setSongTitle(e.target.value)} />
             <input type="date" value={releaseDate} onChange={e => setReleaseDate(e.target.value)} />
@@ -57,50 +46,6 @@ const UploadForm = () => {
             <input type="file" onChange={e => setArtwork(e.target.files[0])} />
             <button type="submit">Upload</button>
         </form>
-=======
-        <div className="upload-form">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3">
-                    <Form.Label>Song Title:</Form.Label>
-                    <Form.Control type="text" value={songTitle} onChange={(e) => setSongTitle(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>Release Date:</Form.Label>
-                    <Form.Control type="date" value={releaseDate} onChange={(e) => setReleaseDate(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>Label Copy Text:</Form.Label>
-                    <Form.Control as="textarea" value={labelCopyText} onChange={(e) => setLabelCopyText(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>Release Instructions:</Form.Label>
-                    <Form.Control as="textarea" value={releaseInstructions} onChange={(e) => setReleaseInstructions(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group controlId="audioFile" className="mb-3">
-                    <Form.Label>Audio File:</Form.Label>
-                    <Form.Control type="file" accept=".mp3,.wav,.mpeg" onChange={(e) => setAudioFile(e.target.files[0])} />
-                </Form.Group>
-
-                <Form.Group controlId="labelCopyPDF" className="mb-3">
-                    <Form.Label>Label Copy PDF:</Form.Label>
-                    <Form.Control type="file" accept=".pdf" onChange={(e) => setLabelCopyPDF(e.target.files[0])} />
-                </Form.Group>
-
-                <Form.Group controlId="artwork" className="mb-3">
-                    <Form.Label>Artwork:</Form.Label>
-                    <Form.Control type="file" accept="image/*" onChange={(e) => setArtwork(e.target.files[0])} />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Button variant="primary" type="submit">Submit</Button>
-                </Form.Group>
-            </Form>
-        </div>
->>>>>>> tmp
     );
 };
 
