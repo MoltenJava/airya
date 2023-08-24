eb9fc8c2b3aed77ca4b9db003274e0ce9355e4a0
MoltenJava <arya@shapouri.com>
2023-08-23 18:44:35 -0700

    postgres fix

--- app_listener.py
+++ app_listener.py
@@ -15,6 +15,8 @@ CORS(app)
 
 # Configuring database connection
 DATABASE_URL = os.environ['DATABASE_URL']
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
